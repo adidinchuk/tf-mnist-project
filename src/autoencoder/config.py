b93129e529b6e294c9c96fa7f79cc3e8fb3d9970
Andrew Didinchuk <adidinchuk@gmail.com>
2019-11-13 21:41:16 -0500

    updated reference paths to use root as base

--- src/autoencoder/config.py
+++ src/autoencoder/config.py
@@ -1,7 +1,7 @@
 batch_size = 50
 epochs = 500
-data_path = '../../data'
+data_path = 'data'
 train_labels = '/train-labels.idx1-ubyte'
 train_images = '/train-images.idx3-ubyte'
-checkpoint_path = "../../models/autoencoder/checkpoints/cp.ckpt"
-final_model_path = "../../models/autoencoder/production/model.pb"
+checkpoint_path = "models/autoencoder/checkpoints/cp.ckpt"
+final_model_path = "/models/autoencoder/production/model.pb"
--- src/classifier/config.py
+++ src/classifier/config.py
@@ -1,8 +1,8 @@
 batch_size = 50
 epochs = 100
-data_path = '../../data'
+data_path = 'data'
 train_labels = '/train-labels.idx1-ubyte'
 train_images = '/train-images.idx3-ubyte'
-autoencoder_model_path = "../../models/autoencoder/production/model.pb"
-checkpoint_path = "../../models/classifier/checkpoints/cp.ckpt"
-final_model_path = "../../models/classifier/production/model.pb"
+autoencoder_model_path = "models/autoencoder/production/model.pb"
+checkpoint_path = "models/classifier/checkpoints/cp.ckpt"
+final_model_path = "models/classifier/production/model.pb"
